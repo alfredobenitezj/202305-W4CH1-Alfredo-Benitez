277c1b5733e31c3863d358902bf183d1f5bd1db5
Alfredo <alfredobenitezjuanilla@gmail.com>
2023-05-22 22:38:25 +0200

    Add mock and button and create carpet

---
+++ src/data/button.tsx
@@ -0,0 +1,8 @@
+export function button() {
+  return (
+    <section className="button">
+      <p className="info">gentlemen pointing at you</p>
+      <button className="button--select">Select all</button>
+    </section>
+  );
+}
--- src/data/mock.tsx
+++ src/data/mock.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export type gentlemendata = {
+export type gentlemenData = {
   name: string;
   status: string;
   profession: string;
