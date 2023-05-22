8f5900b9e219a55be470913dfba46060a774a603
Alfredo <alfredobenitezjuanilla@gmail.com>
2023-05-22 20:41:40 +0200

    Add function array

--- src/header.tsx
+++ src/header.tsx
@@ -3,16 +3,14 @@ type PropsType = {
   user: string;
 };
 
-export function Header({ label, user }: PropsType) {
+export function Header() {
   const title = "The Pointing gentlement";
 
   return (
-    <>
+ <>
       <header className="header">
-        <h1>{label}</h1>
-      </header>
-      <p>{user}</p>
+      
       <p>{title.toLocaleUpperCase()}</p>
     </>
-  );
-}
+  )
+  }
